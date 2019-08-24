c6278760c8c1084b73ee7ab57713f1afadb417ca
mariahv <mariahv14@hotmail.com>
2019-08-23 20:24:33 -0500

    diseño-portafolio

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+$(function(){
+  var $jittery = $('.jittery'),
+      aText    = $jittery.text().split(''),
+      letters = '';
+
+  for(var i = 0; i < aText.length; i++){
+    letters += '<span>'+aText[i]+'</span>';
+  }
+
+  $jittery.empty().append(letters);
+
+  $.each($('span', $jittery), function(i){
+    $(this).css('animation-delay', '-'+i+'70ms');
+  });
+});
